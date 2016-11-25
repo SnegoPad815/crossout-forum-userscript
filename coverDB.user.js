5dd3613c9e223d8c3535f1e96cf36b0d9c6e6b98
SnegoPad815 <SnegoPad815@mail.ru>
2016-11-26 03:08:53 +0400

    Update coverDB.user.js

--- coverDB.user.js
+++ coverDB.user.js
@@ -1,13 +1,8 @@
 var covers = [
 {
 	"id": 30576,
-	"top": 180,
-	"url": "http://img03.deviantart.net/e43b/i/2015/120/e/4/nuclear_winter__apocalypse_by_ladyelenanaylor-d38q2ak.jpg"
-},
-{
-	"id": 197,
-	"top": 125,
-	"url": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1N6f7Y8cgnjYrBPLIYRmytGRHZk5TsE0wEYQECcbEw73VOQaN"
+	"top": 226,
+	"url": "https://img03.deviantart.net/e43b/i/2015/120/e/4/nuclear_winter__apocalypse_by_ladyelenanaylor-d38q2ak.jpg"
 }
 
 ]
